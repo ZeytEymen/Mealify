9eadf228cef62fc5facb95425772b89209b71150
Zeyt Eymen <zeyteymen@gmail.com>
2024-03-21 15:56:37 +0300

    Updating FrontEnd; Added PopUp modals last change Companies Delete

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -6,7 +6,16 @@ using Mealify.Data;
 using Mealify.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mealify.Data;
+using Mealify.Models;
 
 namespace Mealify.Controllers
 {
@@ -23,9 +32,56 @@ namespace Mealify.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var companies = _context.Companies.Include(c => c.State).Where(c => c.StateId == 1).ToList();
+            var companies = _context.Companies.Include(c => c.State).Include(c => c.ParentCompany).ToList();
             return View(companies);
         }
+
+        public ActionResult Edit(int? id)
+        {
+            var company =  _context.Companies!.FindAsync(id).Result;
+            if (company == null)
+                return NotFound();
+            
+            ViewData["StateId"] = new SelectList(_context.Set<State>(), "Id", "Name", company.StateId);
+            ViewData["ParentCompanyId"] = new SelectList(_context.Set<Company>(), "Id", "Name", company.ParentCompanyId);
+            return View(company);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Company company)
+        {
+           //var oldCompany = _context.Companies!.Where(r => r.Id == company.Id).FirstOrDefault();
+            company.RegisterDate = DateTime.Now;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Update(company);
+                    _context.SaveChanges();
+                    return Ok("ok");
+                }
+                else
+                {
+                    var errorMessages = ModelState.Values.SelectMany(v => v.Errors)
+                                 .Select(e => e.ErrorMessage)
+                                 .ToList();
+                    var errorMessage = string.Join(" ", errorMessages);
+
+                    // Hata mesajlarını ViewBag üzerinden View'a aktarabiliriz
+                    return Ok(errorMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
     }
 }
 
